df77a8a589c3facca40a04a09fcf2c14f05ae704
Ranju <ranju@b-eagles.com>
2014-07-17 18:04:50 +0530

    Finished markup for new patron page

--- handlers/index.py
+++ handlers/index.py
@@ -9,4 +9,10 @@ class HomepageHandler(webapp2.RequestHandler):
         index_path = os.path.join(os.path.dirname(__file__), '../templates/index.html')
         self.response.out.write(template.render(index_path, None))
 
-app = webapp2.WSGIApplication([('/', HomepageHandler)])
+class NewPatronHandler(webapp2.RequestHandler):
+    def get(self):
+        index_path = os.path.join(os.path.dirname(__file__), '../templates/new_patron.html')
+        self.response.out.write(template.render(index_path, None))
+
+app = webapp2.WSGIApplication([('/', HomepageHandler),
+                               ('/new_patron', NewPatronHandler)])

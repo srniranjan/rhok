27a8d52d8ca26b85e320ad2058b565cc84ef8626
Ranju <ranju@b-eagles.com>
2014-08-12 19:06:35 +0530

    Added a list and delete functionality

--- handlers/index.py
+++ handlers/index.py
@@ -53,7 +53,38 @@ class SendRemindersHandler(webapp2.RequestHandler):
                 logging.info('Running ' + reminder.name + ' at ' + reminder.patron.phone)
                 logging.info('+++++++++++++++')
 
+class PatronValObj():
+    def __init__(self, patron):
+        self.name = patron.name
+        self.phone = patron.phone
+        self.address = patron.address
+        self.tasks = ''
+        for task_str in patron.tasks.split('####')[1:]:
+            task_name, times = task_str.split('~~~~')
+            self.tasks += task_name + ' at ' + times + ', '
+        self.patron_id = patron.key().id()
+
+class ListPatronsHandler(webapp2.RequestHandler):
+    def get(self):
+        patrons = Patron.all()
+        val_objs = []
+        for patron in patrons.run(limit=1000):
+            val_objs.append(PatronValObj(patron))
+        index_path = os.path.join(os.path.dirname(__file__), '../templates/list_patrons.html')
+        self.response.out.write(template.render(index_path, {'patrons':val_objs}))
+
+class DelPatronHandler(webapp2.RequestHandler):
+    def post(self):
+        patron = Patron.get_by_id(long(self.request.get('patron_id')))
+        reminders = Reminder.all()
+        reminders.filter("patron =", patron)
+        for reminder in reminders.run(limit=1000):
+            reminder.delete()
+        patron.delete()
+
 app = webapp2.WSGIApplication([('/', HomepageHandler),
                                ('/new_patron', NewPatronHandler),
                                ('/save_patron',SavePatronHandler),
-                               ('/send_reminders',SendRemindersHandler)])
+                               ('/send_reminders',SendRemindersHandler),
+                               ('/list_patrons',ListPatronsHandler),
+                               ('/del_patron',DelPatronHandler)])
